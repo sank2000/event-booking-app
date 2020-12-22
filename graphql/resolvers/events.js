083d14ab2f626e2e5f8b8c2aef3b9597d9b47e7e
sank2000 <santhoshvelr@yahoo.com>
2020-12-22 20:52:26 +0530

    code refactored and booking added

---
+++ graphql/resolvers/events.js
@@ -0,0 +1,42 @@
+const Event = require('../../models/event');
+
+const { transformEvent } = require('./merge');
+
+module.exports = {
+  events: async () => {
+    try {
+      const events = await Event.find();
+      return events.map(event => {
+        return transformEvent(event);
+      });
+    } catch (err) {
+      throw err;
+    }
+  },
+  createEvent: async args => {
+    const event = new Event({
+      title: args.eventInput.title,
+      description: args.eventInput.description,
+      price: +args.eventInput.price,
+      date: new Date(args.eventInput.date),
+      creator: '5fe1d26c6022817428f1b079'
+    });
+    let createdEvent;
+    try {
+      const result = await event.save();
+      createdEvent = transformEvent(result);
+      const creator = await User.findById('5fe1d26c6022817428f1b079');
+
+      if (!creator) {
+        throw new Error('User not found.');
+      }
+      creator.createdEvents.push(event);
+      await creator.save();
+
+      return createdEvent;
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+  }
+};
