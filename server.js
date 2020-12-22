3737133d811b42e0577bf04484512443622ba5de
sank2000 <santhoshvelr@yahoo.com>
2020-12-22 16:38:33 +0530

    user relation added

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 
 const { graphqlHTTP } = require('express-graphql');
-const { buildSchema } = require('graphql');
 
-const Event = require('./models/event');
+const graphQlSchema = require('./graphql/schema');
+const graphQlResolvers = require('./graphql/resolvers');
 
 const app = express();
 const port = Number(process.env.PORT);
@@ -24,66 +24,8 @@ app.use(cors());
 app.use(
   '/graphql',
   graphqlHTTP({
-    schema: buildSchema(`
-
-        type Event {
-          _id: ID!
-          title: String!
-          description: String!
-          price: Float!
-          date: String!
-        }
-
-        input EventInput {
-          title: String!
-          description: String!
-          price: Float!
-          date: String!
-        }
-
-        type RootQuery {
-            events: [Event!]!
-        }
-        type RootMutation {
-            createEvent(eventInput: EventInput): Event
-        }
-
-        schema {
-            query: RootQuery
-            mutation: RootMutation
-        }
-    `),
-    rootValue: {
-      events: () => {
-        return Event.find()
-          .then(events => {
-            return events.map(event => {
-              return { ...event._doc, _id: event.id };
-            });
-          })
-          .catch(err => {
-            throw err;
-          });
-      },
-      createEvent: args => {
-        const event = new Event({
-          title: args.eventInput.title,
-          description: args.eventInput.description,
-          price: +args.eventInput.price,
-          date: new Date(args.eventInput.date)
-        });
-        return event
-          .save()
-          .then(result => {
-            console.log(result);
-            return { ...result._doc, _id: result._doc._id.toString() };
-          })
-          .catch(err => {
-            console.log(err);
-            throw err;
-          });
-      }
-    },
+    schema: graphQlSchema,
+    rootValue: graphQlResolvers,
     graphiql: true
   })
 );
