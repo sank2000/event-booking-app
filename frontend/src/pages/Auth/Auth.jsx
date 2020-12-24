608c98470f53834c3466a4f0feb3d0708d0772cd
sank2000 <santhoshvelr@yahoo.com>
2020-12-24 12:59:07 +0530

    login page added

--- frontend/src/pages/Auth/Auth.jsx
+++ frontend/src/pages/Auth/Auth.jsx
@@ -1,9 +1,104 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classes from './style.module.scss';
 
 export default function Auth() {
+  const [isLogin, setIsLogin] = useState(true);
+
+  const [value, setValue] = useState({
+    email: '',
+    password: ''
+  });
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+
+    setValue(old => ({
+      ...old,
+      [name]: value
+    }));
+  };
+
+  const switchModeHandler = () => {
+    setIsLogin(old => !old);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const email = value.email;
+    const password = value.password;
+
+    if (email.trim().length === 0 || password.trim().length === 0) {
+      return;
+    }
+
+    let requestBody = {
+      query: `
+        query {
+          login(email: "${email}", password: "${password}") {
+            userId
+            token
+            tokenExpiration
+          }
+        }
+      `
+    };
+
+    if (!isLogin) {
+      requestBody = {
+        query: `
+          mutation {
+            createUser(userInput: {email: "${email}", password: "${password}"}) {
+              _id
+              email
+            }
+          }
+        `
+      };
+    }
+
+    fetch('http://localhost:8000/graphql', {
+      method: 'POST',
+      body: JSON.stringify(requestBody),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error('Failed!');
+        }
+        return res.json();
+      })
+      .then(resData => {
+        console.log(resData);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
   return (
-    <section className='page'>
-      <h1>The Auth Page</h1>;
+    <section className='fullpage' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <form className={classes.auth_form} onSubmit={handleSubmit}>
+        <div className={classes.form_top}>
+          <img src='/ico/favicon-32x32.png' alt='logo' />
+          <h3>{!isLogin ? 'Signup' : 'Login'}</h3>
+        </div>
+        <div className={classes.form_control}>
+          <label htmlFor='email'>E-Mail</label>
+          <input type='email' id='email' name='email' value={value.email} onChange={handleChange} />
+        </div>
+        <div className={classes.form_control}>
+          <label htmlFor='password'>Password</label>
+          <input type='password' id='password' name='password' value={value.password} onChange={handleChange} />
+        </div>
+        <div className={classes.form_action}>
+          <button type='submit'>Submit</button>
+          <button type='button' onClick={switchModeHandler}>
+            Switch to {isLogin ? 'Signup' : 'Login'}
+          </button>
+        </div>
+      </form>
     </section>
   );
 }
